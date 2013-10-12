85f48ba8b0db9301c44e186d322682e9904c26a6
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-10-11 20:10:10 -0700

    Starting 7 in 7

---
+++ seven_languages_seven_weeks/ruby/day_1.rb
@@ -0,0 +1,11 @@
+# Print the string "Hello, world"
+puts 'Hello, world.'
+
+# For the string "Hello, Ruby", find the index of the word "Ruby"
+"Hello, Ruby".index("Ruby")
+
+# Print your name ten times
+(1..10).each do |i| puts "Yusuke" end
+
+# Print the string "This is sentence number 1", where the number changes from one to 10
+(1..10).each do |i| puts "This is sentence number #{i}" end
