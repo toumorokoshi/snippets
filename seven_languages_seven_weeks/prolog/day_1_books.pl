b2fe7389a55eb0e940a5c28ffe4eca8e5e004cce
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-10-24 22:41:54 -0700

    day 1 prolog

---
+++ seven_languages_seven_weeks/prolog/day_1_books.pl
@@ -0,0 +1,22 @@
+% Books
+
+book(enders_game, card).
+book(ready_player_one, cline).
+book(prisoner_of_azkaban, rowling).
+book(fellowship_of_the_ring, tolkien).
+book(the_two_towers, tolkien).
+book(return_of_the_king, tolkien).
+
+% to query all books from an author:
+% book(What, tolkien).
+
+% Musicians
+
+plays(flea, bass).
+plays(slash, guitar).
+plays(sting, bass).
+plays(john_mayer, guitar).
+plays(jack_johnson, guitar).
+
+% query all musicians who play guitar
+% plays(What, guitar).
