4f13d197e3fae21afff86c4500f0c6d26db171f6
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-11-06 20:09:28 -0800

    got count_words working

--- seven_languages_seven_weeks/erlang/day_1.erl
+++ seven_languages_seven_weeks/erlang/day_1.erl
@@ -1,15 +1,30 @@
 -module(day_1).
 -export([count_words/1, test/1, count_to_ten/0, success_or_error/1]).
-
+-include_lib("eunit/include/eunit.hrl").
 
 count_words([]) -> 0;
-count_words([C1, C2 | T]) ->
+count_words([C1]) ->
 if
-    (C1 /= $ ) and (C2 /= $ ) ->
-        0;
+    C1 /= $  ->
+        1;
     true ->
         0
-end + count_words(T).
+end
+;
+count_words([C1, C2 | T]) ->
+if
+    (C1 /= $ ) and (C2 == $ ) ->
+        1;
+    true ->
+        0
+end + count_words([C2 | T]).
+
+% tests
+count_words_test() -> 2 = count_words("hello world").
+count_no_words_test() -> 0 = count_words("").
+count_one_word_test() -> 1 = count_words("hello").
+count_one_word_mutiple_whitespace_test() -> 2 = count_words("hello  world").
+count_one_word_trailing_leading_whitespace_test() -> 1 = count_words("  hello ").
 
 test([C|_]) ->
     if
