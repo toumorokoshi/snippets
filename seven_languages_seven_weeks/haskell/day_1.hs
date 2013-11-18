eb508af56aa646f956ab74cafeea34b099439a87
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-11-18 00:48:41 -0800

    halfway done with day 1 of Haskell

---
+++ seven_languages_seven_weeks/haskell/day_1.hs
@@ -0,0 +1,17 @@
+module Day1 where
+-- implementation book
+       allEven :: [Integer] -> [Integer]
+       allEven [] = []
+       allEven (h:t) = if even h then h:allEven t else allEven t
+-- implementation with list comprehension
+       allEven2 :: [Integer] -> [Integer]
+       allEven2 list = [ (x) | x <- list, rem x 2 == 0]
+-- reverses list
+       myReverse :: [t] -> [t]
+       myReverse [] = []
+       myReverse (h:t) = myReverse(t) ++ [h]
+-- build two-tuples with multiple color combinations
+       colors = ["Blue", "Black", "Red"]
+       colorCombinations = [(c1, c2) | c1 <- colors, c2 <- colors, c1 < c2]
+-- multiplication table
+       multiplication = [(n1, n2, n1 * n2) | n1 <- [1..12], n2 <- [1..12], n1 <= n2]
