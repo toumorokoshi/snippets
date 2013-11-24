87b306bf4bd79094dd12b303a382d1bbb6dd4c4d
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-11-23 17:00:29 -0800

    got custom sort + string to number conversion

--- seven_languages_seven_weeks/haskell/day_2.hs
+++ seven_languages_seven_weeks/haskell/day_2.hs
@@ -1,8 +1,13 @@
 module Day2 where
+    import Data.Char
 -- quicksort-ish
-    mySort :: Ord n => [n] -> [n]
-    mySort [] = []
-    mySort (p:list) =
-        let lesser = [ x | x <- list, x <= p]
-            greater = [ x | x <- list, x > p]
-        in (mySort lesser) ++ [p] ++ (mySort greater)
+    mySortBy :: (a -> a-> Ordering) -> [a] -> [a]
+    mySortBy cmp [] = []
+    mySortBy cmp (p:list) =
+        let lesser = [ x | x <- list, x `cmp` p == LT]
+            greater = [ x | x <- list, x `cmp` p /= LT]
+        in (mySortBy cmp lesser) ++ [p] ++ (mySortBy cmp greater)
+    mySort :: (Ord a) => [a] -> [a]
+    mySort = mySortBy compare
+    stringToNum :: [Char] -> Int
+    stringToNum rawNum = foldl (\y x -> y * 10 + x) 0 [ Data.Char.digitToInt x | x <- rawNum, x >= '0', x <= '9' ]
