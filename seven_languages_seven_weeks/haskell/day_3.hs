ab9fd95b22bee803b4d0c6dc8ab5f9b3e5803ff0
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-12-01 02:21:17 -0800

    started on maze

--- seven_languages_seven_weeks/haskell/day_3.hs
+++ seven_languages_seven_weeks/haskell/day_3.hs
@@ -13,3 +13,17 @@ module Day3 where
     deepLookup key1 key2 table = myLookup key1 table >>= myLookup key2
     deepExample = [("a", [("a1", "value")]),
                    ("b", [("b1", "valueb1")])]
+--    data Node = NodePath ((Int, Int), [Node]) | TerminalNode (Int, Int)
+    data Exits = North | West | East | South deriving Show
+    data Node = NodePath (Int, Int) [Exits] | TerminalNode deriving Show
+    type Maze = [[Node]]
+    maze :: Maze
+    maze = [
+     [ (NodePath (0,0) [South]), (NodePath (1, 0) []), (NodePath (2, 0) []) ],
+     [ (NodePath (0,1) [East]), (NodePath (1, 1) [East]), (NodePath (2, 1) [North, South]) ],
+     [ (NodePath (0,2) []), (NodePath (1, 2) []), (TerminalNode) ]
+           ]
+    getNode :: Maze -> (Int, Int)-> Node
+
+    solveMaze :: [Node]
+    solveMaze =
