4965dbeba198ac75eeb0f9f6df8c3decc1b9a2bd
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-11-30 23:47:02 -0800

    got the recursive example

--- seven_languages_seven_weeks/haskell/day_3.hs
+++ seven_languages_seven_weeks/haskell/day_3.hs
@@ -1,7 +1,7 @@
 module Day3 where
 -- hash table monad
     import Data.List
-    myLookup :: [Char] -> [([Char], [Char])] -> Maybe [Char]
+    myLookup :: [Char] -> [([Char], a)] -> Maybe a
     myLookup key [] = Nothing
     myLookup key table =
         let (hash_key, value) = head table
@@ -9,3 +9,7 @@ module Day3 where
           if hash_key == key
              then Just value
              else (myLookup key (tail table))
+    deepLookup :: [Char] -> [Char] -> [([Char], [([Char], a)])] -> Maybe a
+    deepLookup key1 key2 table = myLookup key1 table >>= myLookup key2
+    deepExample = [("a", [("a1", "value")]),
+                   ("b", [("b1", "valueb1")])]
