6bfc309e4159615e5ae95983fafbbe76e7563b68
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2016-06-27 12:40:34 -0700

    adding final number solver.

--- games/number_solver.py
+++ games/number_solver.py
@@ -1,4 +1,65 @@
-import pprint
+def get_scores_with_optimal_play(array):
+    # we do a bfs traversal, starting with the
+    # final states (only one option left),
+    # and updating every existing permutation
+    # with the optimal choice.
+    optimal_score_by_state = {}
+    states_in_next_round = set()
+
+    # O(n)
+    for idx, i in enumerate(array):
+        state = State(array, idx, idx)
+        scores = [0, 0]
+        scores[state.current_player] += i
+        optimal_score_by_state[state] = scores
+        states_in_next_round.add(state)
+
+    # n iterations, runs once per potential array size
+    while len(states_in_next_round) > 0:
+        states_in_current_round = states_in_next_round
+        states_in_next_round = set()
+        # < n iterations, runs once per potential configuration
+        # of a contigous array of m items.
+        for s in states_in_current_round:
+            optimal_score = optimal_score_by_state[s]
+
+            # O(1) (2 times, once per operation)
+            def _try_state(left_index, right_index, score_modifier_method):
+                try:
+                    state_to_try = State(array, left_index, right_index)
+                    potential_optimal_score = list(optimal_score)
+                    current_player = state_to_try.current_player
+                    potential_optimal_score[current_player] += (
+                        getattr(state_to_try, score_modifier_method)
+                    )
+
+                    if state_to_try not in optimal_score_by_state:
+                        optimal_score_by_state[state_to_try] = potential_optimal_score
+                        states_in_next_round.add(state_to_try)
+                    else:
+                        existing_optimal_score = optimal_score_by_state[state_to_try]
+                        if existing_optimal_score[current_player] <\
+                        potential_optimal_score[current_player]:
+                            optimal_score_by_state[state_to_try] = potential_optimal_score
+                except AssertionError:
+                    pass
+
+            # we try the states that would result in this one:
+            # - removing the beginning of this arry, with left_index - 1
+            _try_state(s.left_index - 1, s.right_index, "left")
+            # - removing the end of this array, with right_index - 1
+            _try_state(s.left_index, s.right_index + 1, "right")
+
+    # now that we have the optimal action for every state, and the
+    # points that are earned when following that plan,
+    # we can return back the root node, to see, with optimal play,
+    # how much each can possibly earn.
+
+    # total complexity:
+    # sum(i) from i=0 to i=n => n^2 / 2
+    # O(n^2)
+    return optimal_score_by_state[State(array, 0, len(array) - 1)]
+
 
 class State:
 
@@ -40,63 +101,5 @@ class State:
         return str((self.left_index, self.right_index))
 
 
-def _add_optimal(optimal_scores, state, potential_optimal_score, player):
-    if state not in optimal_scores:
-        optimal_scores[state] = potential_optimal_score
-    else:
-        existing_optimal_score = optimal_scores[state]
-        if existing_optimal_score[player] < \
-           potential_optimal_score[player]:
-            optimal_scores[state] = potential_optimal_score
-
-
-def get_scores(array):
-    """
-    get the winner for this number game.
-    assumptions:
-
-    - player 1 always goes first.
-    """
-    optimal_scores = {}
-    states_left_to_process = []
-    # first we calculate all the base nodes.
-    for idx, i in enumerate(array):
-        state = State(array, idx, idx)
-        scores = [0, 0]
-        scores[state.current_player] += i
-        optimal_scores[state] = scores
-        states_left_to_process.append(state)
-
-    # bfs search
-    while len(states_left_to_process) > 0:
-        s = states_left_to_process.pop(0)
-        optimal_score = optimal_scores[s]
-        # try permutations
-        try:
-            state_to_try = State(s.array, s.left_index - 1, s.right_index)
-            current_player = state_to_try.current_player
-            potential_optimal_score = list(optimal_score)
-            potential_optimal_score[current_player] += state_to_try.left
-            _add_optimal(optimal_scores, state_to_try,
-                         potential_optimal_score, current_player)
-            if state_to_try not in states_left_to_process:
-                states_left_to_process.append(state_to_try)
-        except AssertionError:
-            continue
-        try:
-            state_to_try = State(s.array, s.left_index, s.right_index + 1)
-            current_player = state_to_try.current_player
-            potential_optimal_score = list(optimal_score)
-            potential_optimal_score[current_player] += state_to_try.right
-            _add_optimal(optimal_scores, state_to_try,
-                         potential_optimal_score, current_player)
-            if state_to_try not in states_left_to_process:
-                states_left_to_process.append(state_to_try)
-        except AssertionError:
-            continue
-
-    pprint.pprint(optimal_scores)
-    return optimal_scores[State(array, 0, len(array) - 1)]
-
-assert get_scores([0, 1, 2, 3, 4]) == [6, 4]
-# assert get_scores([1, 5, 2, 5, 1]) == [4, 10]
+assert get_scores_with_optimal_play([0, 1, 2, 3, 4]) == [6, 4]
+assert get_scores_with_optimal_play([1, 5, 2, 5, 1]) == [4, 10]
